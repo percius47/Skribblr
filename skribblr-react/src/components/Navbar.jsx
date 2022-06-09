f00eba063639335839a8e442ee11592ff97e2ebf
percius47 <33738242+percius47@users.noreply.github.com>
2022-06-09 17:50:47 +0530

    refactor: login page

--- skribblr-react/src/components/Navbar.jsx
+++ skribblr-react/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom"
 // height 5rem
 function Navbar() {
   return (
-    <div className="nav">
+    <div className="__nav">
     <div className="nav-logo" >
   <NavLink to="/" className="logo-link">
      <h1 className='logo-title'>Skribblr.</h1>
